1630494a2f6937408350114f35a25c9fc8a894ca
Fatih Erikli <fatiherikli@gmail.com>
2012-10-09 22:20:26 +0300

    refactored site specific scripts

--- web/dbpatterns/dbpatterns/static/js/utilities.js
+++ web/dbpatterns/dbpatterns/static/js/utilities.js
@@ -1,7 +1,11 @@
 !(function ($) {
+
+    // auto toggle manipulation
     $(function () {
         $("a[data-toggle]").click(function () {
             $($(this).attr("href")).toggle();
+            return false;
         });
     });
+
 })(window.jQuery);
