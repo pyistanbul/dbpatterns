cd6e6502feef230a4795255ad32943c9366a4aeb
Fatih Erikli <fatiherikli@gmail.com>
2012-10-25 04:49:10 +0300

    removed print statement from keyword extractor method

--- web/dbpatterns/documents/utils.py
+++ web/dbpatterns/documents/utils.py
@@ -5,17 +5,17 @@ from django.core.urlresolvers import reverse
 wordnet_lemmatizer = WordNetLemmatizer()
 lancaster_stemmer = LancasterStemmer()
 
-def extract_keywords(title):
 
+def extract_keywords(title):
     original_keywords = [keyword.lower() for keyword in re.split('\W+', title)]
 
     lemmatized_keywords =  map(wordnet_lemmatizer.lemmatize, original_keywords)
 
     stemmed_keywords = map(lancaster_stemmer.stem, original_keywords)
 
-    print stemmed_keywords
-
-    return list(set(original_keywords + lemmatized_keywords + stemmed_keywords))
+    return list(set(original_keywords
+                    + lemmatized_keywords
+                    + stemmed_keywords))
 
 
 def reverse_tastypie_url(resource_name, pk):
