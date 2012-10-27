26010800fc3a9a076e52aed58c0ca7d16b20ff78
Fatih Erikli <fatiherikli@gmail.com>
2012-10-27 14:09:21 +0300

    added "fork of ..." button

--- web/dbpatterns/auth/models.py
+++ web/dbpatterns/auth/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AnonymousUser
 class AnonymousProfile(AnonymousUser):
     username = "AnonymousUser"
     email = ""
+
+    def get_full_name(self):
+        return self.username
