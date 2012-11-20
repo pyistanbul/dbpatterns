f787d73ddbf98c2e33b3c47dd3cc7a57b4144778
Fatih Erikli <fatiherikli@gmail.com>
2012-11-21 00:06:48 +0200

    separated actual urls and legacy urls

--- web/dbpatterns/dbpatterns/urls.py
+++ web/dbpatterns/dbpatterns/urls.py
@@ -1,20 +1,26 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from documents.views import HomeView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    # registration
-    url(r'^accounts/', include('auth.urls')),
+
+    url(r'^$', HomeView.as_view(), name='home'),
 
     # documents
-    url(r'^', include('documents.urls')),
+    url(r'^documents/', include('documents.urls')),
+
+    # registration
+    url(r'^accounts/', include('auth.urls')),
 
     # api
     url(r'^api/', include('api.urls')),
 
+    # legacy urls
+    url(r'^', include('documents.legacy_urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
--- web/dbpatterns/documents/urls.py
+++ web/dbpatterns/documents/urls.py
@@ -1,35 +1,21 @@
 from django.conf.urls import patterns, url
-from django.views.generic import RedirectView
 
-from documents.views import (HomeView, DocumentDetailView, ExportDocumentView, DocumentForksView,
+from documents.views import (DocumentDetailView, ExportDocumentView, DocumentForksView,
                              DocumentStarsView, NewDocumentView, MyDocumentsView, DocumentEditView,
                              ForkDocumentView, StarDocumentView, SearchDocumentView)
 
 urlpatterns = patterns('',
 
-    url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^documents/$', MyDocumentsView.as_view(), name='my_documents'),
-    url(r'^documents/search$', SearchDocumentView.as_view(), name='search_document'),
-    url(r'^documents/new$', NewDocumentView.as_view(), name='new_document'),
-    url(r'^documents/(?P<slug>[-\w]+)/$', DocumentDetailView.as_view(), name='show_document'),
-    url(r'^documents/(?P<slug>[-\w]+)/forks$', DocumentForksView.as_view(), name='show_document_forks'),
-    url(r'^documents/(?P<slug>[-\w]+)/stars', DocumentStarsView.as_view(), name='show_document_stars'),
-    url(r'^documents/(?P<slug>[-\w]+)/export/(?P<exporter>[-\w]+)$', ExportDocumentView.as_view(), name='export_document'),
-    url(r'^documents/(?P<slug>[-\w]+)/edit$', DocumentEditView.as_view(), name='edit_document'),
-    url(r'^documents/(?P<slug>[-\w]+)/fork$', ForkDocumentView.as_view(), name='fork_document'),
-    url(r'^documents/(?P<slug>[-\w]+)/star$', StarDocumentView.as_view(), name='star_document'),
+    url(r'^$', MyDocumentsView.as_view(), name='my_documents'),
+    url(r'^search$', SearchDocumentView.as_view(), name='search_document'),
+    url(r'^new$', NewDocumentView.as_view(), name='new_document'),
+    url(r'^(?P<slug>[-\w]+)/$', DocumentDetailView.as_view(), name='show_document'),
+    url(r'^(?P<slug>[-\w]+)/forks$', DocumentForksView.as_view(), name='show_document_forks'),
+    url(r'^(?P<slug>[-\w]+)/stars', DocumentStarsView.as_view(), name='show_document_stars'),
+    url(r'^(?P<slug>[-\w]+)/export/(?P<exporter>[-\w]+)$', ExportDocumentView.as_view(), name='export_document'),
+    url(r'^(?P<slug>[-\w]+)/edit$', DocumentEditView.as_view(), name='edit_document'),
+    url(r'^(?P<slug>[-\w]+)/fork$', ForkDocumentView.as_view(), name='fork_document'),
+    url(r'^(?P<slug>[-\w]+)/star$', StarDocumentView.as_view(), name='star_document'),
 
-
-    # Legacy URLs
-    url(r'^my-documents/$', RedirectView.as_view(url="/documents/")),
-    url(r'^search/$', RedirectView.as_view(url="/documents/search")),
-    url(r'^new/$', RedirectView.as_view(url="/documents/new")),
-    url(r'^show/(?P<slug>[-\w]+)/$', RedirectView.as_view(url="/documents/%(slug)s/")),
-    url(r'^show/(?P<slug>[-\w]+)/forks$', RedirectView.as_view(url="/documents/%(slug)s/forks")),
-    url(r'^show/(?P<slug>[-\w]+)/stars', RedirectView.as_view(url="/documents/%(slug)s/stars")),
-    url(r'^edit/(?P<slug>[-\w]+)/', RedirectView.as_view(url="/documents/%(slug)s/edit")),
-    url(r'^export/(?P<slug>[-\w]+)/(?P<exporter>[-\w]+)', RedirectView.as_view(url="/documents/%(slug)s/export/%(exporter)s")),
-    url(r'^fork/(?P<slug>[-\w]+)/$', RedirectView.as_view(url="/documents/%(slug)s/fork")),
-    url(r'^star/(?P<slug>[-\w]+)/$', RedirectView.as_view(url="/documents/%(slug)s/star")),
 
 )
