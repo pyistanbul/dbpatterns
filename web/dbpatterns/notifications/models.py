e9de2b75d8d5151f3f5da7cccb9ba0a861a92676
Fatih Erikli <fatiherikli@gmail.com>
2013-01-10 01:30:50 +0200

    cleanup on the notifications model

--- web/dbpatterns/notifications/models.py
+++ web/dbpatterns/notifications/models.py
@@ -14,18 +14,45 @@ from notifications.constants import (NOTIFICATION_TYPE_COMMENT, NOTIFICATION_TYP
                                      NOTIFICATION_TEMPLATES)
 
 class NotificationManager(object):
-
+    """
+    A class that allows create, edit, read notifications.
+    """
     collection = get_collection("notifications")
 
     def filter_by_user_id(self, user_id):
+        """
+        Shows the notifications of user.
+        """
         return self.collection.find({
             "recipient": user_id,
         })
 
     def mark_as_read(self, user_id):
+        """
+        Marks as read the notifications of user.
+        """
         self.collection.update(
             { "recipient": user_id, "is_read": False },
             { "$set": { "is_read": True }}, multi=True)
+
+    def create(self, object_id, notification_type, sender, recipient_id):
+        """
+        Creates notification from provided parameters.
+        """
+
+        if not sender.id == recipient_id: # if the sender affect the own document,
+                                          # ignore it.
+            self.collection.insert({
+                "is_read": False,
+                "object_id": object_id,
+                "type": notification_type,
+                "date_created": datetime.today(),
+                "sender": {
+                    "username": sender.username,
+                    "email": sender.email # it's required for gravatar
+                },
+                "recipient": recipient_id
+            })
 
 class Notification(dict):
     """
@@ -61,63 +88,52 @@ class Notification(dict):
 
 @receiver(comment_done)
 def create_comment_notification(instance, **kwargs):
-    newsfeed = get_collection("notifications")
-    newsfeed.insert({
-        "is_read": False,
-        "object_id": instance.document._id,
-        "type": NOTIFICATION_TYPE_COMMENT,
-        "date_created": datetime.today(),
-        "sender": {
-            "username": instance.user.username,
-            "email": instance.user.email # it's required for gravatar
-        },
-        "recipient": instance.document.user_id
-    })
+    """
+    Sends notification to the user of commented document.
+    """
+    Notification.objects.create(
+        object_id=instance.document._id,
+        notification_type=NOTIFICATION_TYPE_COMMENT,
+        sender=instance.user,
+        recipient_id=instance.document.user_id
+    )
 
 @receiver(fork_done)
 def create_fork_notification(instance, **kwargs):
-    newsfeed = get_collection("notifications")
+    """
+    Sends notification to the user of forked document.
+    """
     forked_document = Document.objects.get(
             _id=ObjectId(instance.fork_of))
-    newsfeed.insert({
-        "is_read": False,
-        "object_id": instance._id,
-        "type": NOTIFICATION_TYPE_FORK,
-        "date_created": instance.date_created,
-        "sender": {
-            "username": instance.user.username,
-            "email": instance.user.email # it's required for gravatar
-        },
-        "recipient": forked_document.user_id
-    })
+
+    Notification.objects.create(
+        object_id=instance._id,
+        notification_type=NOTIFICATION_TYPE_FORK,
+        sender=instance.user,
+        recipient_id=forked_document.user_id
+    )
 
 
 @receiver(star_done)
 def create_star_notification(instance, user, **kwargs):
-    newsfeed = get_collection("notifications")
-    newsfeed.insert({
-        "is_read": False,
-        "object_id": instance._id,
-        "type": NOTIFICATION_TYPE_STAR,
-        "date_created": instance.date_created,
-        "sender": {
-            "username": user.username,
-            "email": user.email # it's required for gravatar
-        },
-        "recipient": instance.user_id
-    })
+    """
+    Sends notification to the user of starred document.
+    """
+    Notification.objects.create(
+        object_id=instance._id,
+        notification_type=NOTIFICATION_TYPE_STAR,
+        sender=user,
+        recipient_id=instance.user_id
+    )
 
 @receiver(follow_done)
 def create_following_notification(following, follower, **kwargs):
-    newsfeed = get_collection("notifications")
-    newsfeed.insert({
-        "is_read": False,
-        "object_id": follower.id,
-        "type": NOTIFICATION_TYPE_FOLLOWING,
-        "date_created": datetime.now(),
-        "sender": {
-            "username": follower.username,
-            "email": follower.email # it's required for gravatar
-        },
-        "recipient": following.id
-    })
+    """
+    Sends notification to the followed user from the follower.
+    """
+    Notification.objects.create(
+        object_id=follower.id,
+        notification_type=NOTIFICATION_TYPE_FOLLOWING,
+        sender=follower,
+        recipient_id=following.id
+    )
