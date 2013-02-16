4910d81ce1ffcf0811b6b8a28e7b9fb10aa60acb
Fatih Erikli <fatiherikli@gmail.com>
2013-02-16 03:38:56 +0200

    add websocket to document model

--- web/dbpatterns/dbpatterns/static/js/models/document.js
+++ web/dbpatterns/dbpatterns/static/js/models/document.js
@@ -1,27 +1,21 @@
 dbpatterns.models.Document = Backbone.Model.extend({
 
     urlRoot: "/api/documents",
-    initialize: function () {
 
+    initialize: function (model, options) {
         this.entities = new dbpatterns.collections.Entity;
+        this.socket = io.connect(options.socket_uri);
     },
 
     parse: function (result) {
-
-        if (!result) {
-            return result;
-        }
-
+        if (!result) { return result; }
         this.entities.reset(result.entities);
         this.entities.bind("add remove persist", this.persist.bind(this));
         return result;
-
     },
 
     persist: function () {
-        this.set({
-            "entities": this.entities.toJSON()
-        });
+        this.set({"entities": this.entities.toJSON()});
     }
 
 });
