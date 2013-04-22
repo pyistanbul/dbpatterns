bda5faeeb05a2300bdc45a0a68cdfd24c8ce8feb
Özgür Vatansever <ozgur.vatansever@markafoni.com>
2013-04-22 14:48:11 +0300

    mongo authentication small fix

--- web/dbpatterns/documents/__init__.py
+++ web/dbpatterns/documents/__init__.py
@@ -11,12 +11,14 @@ def get_connection():
             host=getattr(settings, 'MONGODB_HOST', None),
             port=getattr(settings, 'MONGODB_PORT', None)
         )
-        db = _connection[settings.MONGODB_DATABASE]
         username = getattr(settings, 'MONGODB_USERNAME', None)
         password = getattr(settings, 'MONGODB_PASSWORD', None)
+        db = _connection[settings.MONGODB_DATABASE]
         if username and password:
             db.authenticate(username, password)
-    return db
+        return db
+    return _connection[settings.MONGODB_DATABASE]
+
 connection = SimpleLazyObject(get_connection)
 
 
