0fc30cce138b489ecd6f5194962ebbb3d320e72b
fatiherikli <fatiherikli@gmail.com>
2013-04-29 23:30:40 +0300

    Fixed the pep8 warnings of comments app

--- web/dbpatterns/comments/models.py
+++ web/dbpatterns/comments/models.py
@@ -10,39 +10,49 @@ from profiles.models import AnonymousProfile
 from documents import get_collection
 from documents.models import Document
 
-class CommentManager(object):
 
+class CommentManager(object):
     def get(self, **kwargs):
         return Comment(get_collection("comments").find_one(kwargs))
 
+
 class Comment(dict):
+    """
+    A model that allows create, read comments from mongodb
+    """
     __getattr__ = dict.get
     objects = CommentManager()
 
     @property
     def profile_url(self):
+        """Return profile url of user"""
         return reverse("auth_profile", args=[self.user.username])
 
     @property
     def avatar_url(self):
+        """Returns the gravar address of email"""
         return gravatar_for_email(self.user.email, size=40)
 
     @property
     def username(self):
+        """Shortcut parameter to username"""
         return self.user.username
 
     @property
     def full_name(self):
+        """Shortcut parameter to full name"""
         return self.user.get_full_name() or None
 
     @property
     def user(self):
+        """Returns cached user"""
         if not self._cached_user:
             self._cached_user = self.get_user()
 
         return self._cached_user
 
     def get_user(self):
+        """Returns the user of comment"""
         try:
             return User.objects.get(id=self.user_id)
         except User.DoesNotExist:
@@ -50,17 +60,22 @@ class Comment(dict):
 
     @property
     def document(self):
+        """Returns the document of comment"""
         return Document(get_collection("documents").find_one({
             "_id": ObjectId(self.document_id)
         }))
 
     @property
     def pk(self):
+        """Shortcut method to deal with underscore restriction
+        of django templates"""
         return self._id
 
     def get_news_type(self):
+        """Returns newsfeed type."""
         return NEWS_TYPE_COMMENT
 
     @property
     def is_public(self):
+        """Returns the visibility of the document of comment"""
         return self.document.is_public
--- web/dbpatterns/comments/resources.py
+++ web/dbpatterns/comments/resources.py
@@ -15,18 +15,22 @@ from comments.models import Comment
 from comments.signals import comment_done, comment_delete
 from documents import get_collection
 
-class CommentResource(MongoDBResource):
 
+class CommentResource(MongoDBResource):
     id = fields.CharField(attribute="_id")
     body = fields.CharField(attribute="body", null=True)
-    document_id = fields.CharField(attribute="document_id", readonly=True, null=True)
-    date_created = fields.DateTimeField(attribute="date_created", readonly=True, null=True)
+    document_id = fields.CharField(
+        attribute="document_id", readonly=True, null=True)
+    date_created = fields.DateTimeField(
+        attribute="date_created", readonly=True, null=True)
 
     # profile specific fields
     user_id = fields.IntegerField(attribute="user_id", readonly=True, null=True)
     username = fields.CharField(attribute="username", readonly=True, null=True)
-    profile_url = fields.CharField(attribute="profile_url", readonly=True, null=True)
-    avatar_url = fields.CharField(attribute="avatar_url", readonly=True, null=True)
+    profile_url = fields.CharField(
+        attribute="profile_url", readonly=True, null=True)
+    avatar_url = fields.CharField(
+        attribute="avatar_url", readonly=True, null=True)
 
     class Meta:
         resource_name = "comments"
@@ -36,6 +40,10 @@ class CommentResource(MongoDBResource):
         object_class = Comment
 
     def dehydrate(self, bundle):
+        """
+        Injects `has_delete_permission` field which indicates the user is
+        authorized to remove comment or not.
+        """
         if bundle.request is not None and bundle.request.user.is_authenticated():
             bundle.data["has_delete_permission"] = \
                 bundle.request.user.pk == bundle.data.get("user_id")
@@ -45,30 +53,43 @@ class CommentResource(MongoDBResource):
         return get_collection("comments")
 
     def obj_get_list(self, request=None, **kwargs):
-
+        """
+        Returns comment of the document
+        """
         if not "document_id" in kwargs:
             return super(CommentResource, self).obj_get_list(request, **kwargs)
 
-        return map(self.get_object_class(), self.get_collection().find({
+        comments = self.get_collection().find({
             "document_id": kwargs.get("document_id")
-        }).sort([['_id', 1]]))
+        }).sort([['_id', 1]])
 
+        return map(self.get_object_class(), comments)
 
     def obj_delete(self, request=None, **kwargs):
+        """
+        - Removes comment if user is authorized
+        - Fires comment_delete signal
+        """
         comment = self.obj_get(**kwargs)
-        if request.user.is_anonymous() \
-            or request.user.pk != comment.get("user_id"):
+        if (request.user.is_anonymous() or
+                not request.user.pk == comment.get("user_id")):
             raise ImmediateHttpResponse(response=http.HttpUnauthorized())
 
         super(CommentResource, self).obj_delete(request, **kwargs)
 
-        comment_delete.send(sender=self,
+        comment_delete.send(
+            sender=self,
             comment_id=comment.id,
             instance=comment
         )
 
     def obj_create(self, bundle, request=None, **kwargs):
-        bundle = super(CommentResource, self).obj_create(bundle,
+        """
+        - Creates new comment
+        - Fires comment_done signal
+        """
+        bundle = super(CommentResource, self).obj_create(
+            bundle,
             user_id=request.user.id,
             document_id=kwargs.get("document_id"),
             date_created=datetime.now()
@@ -77,7 +98,8 @@ class CommentResource(MongoDBResource):
         comment_id = bundle.obj
         comment = Comment.objects.get(_id=ObjectId(comment_id))
 
-        comment_done.send(sender=self,
+        comment_done.send(
+            sender=self,
             comment_id=comment_id,
             instance=comment
         )
@@ -87,7 +109,9 @@ class CommentResource(MongoDBResource):
 
 @receiver(comment_done)
 def comment_on(sender, comment_id, **kwargs):
-
+    """
+    Sends an email to the author of the commented document
+    """
     comment = Comment(get_collection("comments").find_one({
         "_id": ObjectId(comment_id)
     }))
@@ -95,13 +119,14 @@ def comment_on(sender, comment_id, **kwargs):
     document = comment.document
 
     send_mail(
-        subject = "You have new comment(s) on your pattern",
-        message = COMMENT_TEMPLATE % {
+        subject="You have new comment(s) on your pattern",
+        message=COMMENT_TEMPLATE % {
             "document_title": document.title,
             "document_link": settings.SITE_URL + document.get_absolute_url()
         },
-        from_email = settings.COMMENTS_FROM_EMAIL,
-        recipient_list = ['"%s" <%s>' % (
-            document.user.get_full_name() or document.user.username, document.user.email)],
-        fail_silently = True
+        from_email=settings.COMMENTS_FROM_EMAIL,
+        recipient_list=['"%s" <%s>' % (
+            document.user.get_full_name() or document.user.username,
+            document.user.email)],
+        fail_silently=True
     )
